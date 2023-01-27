6f0f74ae1b574b3295450c521b59e09c47d8d8e0
Loureiro32 <gamerhugo68@gmail.com>
2023-01-27 15:04:06 +0000

    serverside

---
+++ olhõtacho/nodejs/teste2.js
@@ -0,0 +1,12 @@
+var mysql = required ('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
