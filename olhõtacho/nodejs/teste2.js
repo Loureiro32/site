f1a478e69a1240fa39148d40cdc1abfd5f3c7afe
Loureiro <46250935+Loureiro32@users.noreply.github.com>
2023-01-27 22:07:01 +0000

    Fix Database

--- olhõtacho/nodejs/teste2.js
+++ olhõtacho/nodejs/teste2.js
@@ -1,9 +1,9 @@
 var mysql = required ('mysql');
 
 var con = mysql.createConnection({
-  host: "",
-  user: "",
-  password: ""
+  host: "Mysql80",
+  user: "root",
+  password: "loureiro123?"
 });
 
 con.connect(function(err) {
