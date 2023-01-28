8e9c8629e1f13d3f65107f28de1ff8a4c632f101
Loureiro <46250935+Loureiro32@users.noreply.github.com>
2023-01-28 12:49:18 +0000

    Update teste2.js

--- olhõtacho/nodejs/teste2.js
+++ olhõtacho/nodejs/teste2.js
@@ -3,7 +3,7 @@ var mysql = required ('mysql');
 var con = mysql.createConnection({
   host: "Mysql80",
   user: "root",
-  password: "loureiro123"
+  password: "loureiro123?"
 });
 
 con.connect(function(err) {
